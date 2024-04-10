a839df6c020de1b4414be5a4289464759fb66c4a
Dan Mindru <1515742+danmindru@users.noreply.github.com>
2024-04-10 21:13:01 +0200

    Delete components/landing directory

--- components/landing/emerald-ai/page.tsx
+++
@@ -1,8 +0,0 @@
-import { EmeraldAi } from './emerald-ai';
-export default function Demo() {
-  return (
-    <div className="flex flex-col w-full">
-      <EmeraldAi />
-    </div>
-  );
-}
--- components/landing/screenshot-two/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ScreenshotTwo } from './screenshot-two';
-
-export default function Demo() {
-  return (
-    <div className="flex flex-col w-full">
-      <ScreenshotTwo />
-    </div>
-  );
-}
