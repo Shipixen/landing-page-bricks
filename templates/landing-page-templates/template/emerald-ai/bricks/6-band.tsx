55e2fc8355bf7d2ccdc9d3c5e4a06267c5b2140f
danmindru <hey@danmindru.dev>
2024-04-15 16:42:40 +0000

    🔄 Sync Landing Page Components

--- templates/landing-page-templates/template/emerald-ai/bricks/6-band.tsx
+++ templates/landing-page-templates/template/emerald-ai/bricks/6-band.tsx
@@ -2,6 +2,21 @@ import { LandingBandSection } from '@/components/landing/LandingBand';
 import { LandingSocialProof } from '@/components/landing/social-proof/LandingSocialProof';
 
 export default function Component() {
+  const avatarItems = [
+    {
+      imageSrc: 'https://picsum.photos/id/64/100/100',
+      name: 'John Doe',
+    },
+    {
+      imageSrc: 'https://picsum.photos/id/65/100/100',
+      name: 'Jane Doe',
+    },
+    {
+      imageSrc: 'https://picsum.photos/id/669/100/100',
+      name: 'Alice Doe',
+    },
+  ];
+
   return (
     <LandingBandSection
       title="4.9/5 stars"
@@ -10,20 +25,7 @@ export default function Component() {
         <LandingSocialProof
           showRating
           numberOfUsers={99}
-          avatarItems={[
-            {
-              imageSrc: 'https://picsum.photos/id/64/100/100',
-              name: 'John Doe',
-            },
-            {
-              imageSrc: 'https://picsum.photos/id/65/100/100',
-              name: 'Jane Doe',
-            },
-            {
-              imageSrc: 'https://picsum.photos/id/669/100/100',
-              name: 'Alice Doe',
-            },
-          ]}
+          avatarItems={avatarItems}
         />
       }
     />
